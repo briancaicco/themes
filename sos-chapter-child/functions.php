7d449ac5071555c3c349436130e5488fa6020b89
Brian Caicco <briancaicco@gmail.com>
2017-07-21 17:58:55 -0400

    woocommerce: changed menu lables to Sessions from Products in chapter theme

--- sos-chapter-child/functions.php
+++ sos-chapter-child/functions.php
@@ -90,48 +90,64 @@ function theme_enqueue_styles() {
 // add_action( 'init', 'change_post_object' );
 
 
-// // Rename Default "Category" Taxonomy to "Topics"
-// //////////////////////////////////////////////////////////////////////
-// function change_cat_label() {
-//     global $submenu;
-//     $submenu['edit.php'][15][0] = 'Topics'; // Rename categories to Topics
-// }
-// add_action( 'admin_menu', 'change_cat_label' );
+// Rename Default "Post" type to "Sessions"
+//////////////////////////////////////////////////////////////////////
+function sos_chapter_change_post_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'Sessions';
+    $submenu['edit.php'][5][0] = 'Sessions';
+    $submenu['edit.php'][10][0] = 'Add Session';
+    $submenu['edit.php'][16][0] = 'Session Tags';
+}
+function sos_chapter_change_post_object() {
+    global $wp_post_types;
+    $labels = &$wp_post_types['product']->labels;
+    $labels->name = 'Sessions';
+    $labels->singular_name = 'Session';
+    $labels->add_new = 'Add Session';
+    $labels->add_new_item = 'Add Session';
+    $labels->edit_item = 'Edit Session';
+    $labels->new_item = 'Session';
+    $labels->view_item = 'View Session';
+    $labels->search_items = 'Search Sessions';
+    $labels->not_found = 'No Sessions found';
+    $labels->not_found_in_trash = 'No Sessions found in Trash';
+    $labels->all_items = 'All Sessions';
+    $labels->menu_name = 'Sessions';
+    $labels->name_admin_bar = 'Sessions';
+}
+ 
+add_action( 'admin_menu', 'sos_chapter_change_post_label' );
+add_action( 'init', 'sos_chapter_change_post_object' );
 
-// function change_cat_object() {
-//     global $wp_taxonomies;
-//     $labels = &$wp_taxonomies['category']->labels;
-//     $labels->name = 'Topic';
-//     $labels->singular_name = 'Topic';
-//     $labels->add_new = 'Add Topic';
-//     $labels->add_new_item = 'Add Topic';
-//     $labels->edit_item = 'Edit Topic';
-//     $labels->new_item = 'Topic';
-//     $labels->view_item = 'View Topic';
-//     $labels->search_items = 'Search Topics';
-//     $labels->not_found = 'No Topics found';
-//     $labels->not_found_in_trash = 'No Topics found in Trash';
-//     $labels->all_items = 'All Topics';
-//     $labels->menu_name = 'Topic';
-//     $labels->name_admin_bar = 'Topic';
-// }
-// add_action( 'init', 'change_cat_object' );
 
-// // Display Post Tags - display using  display_post_tags();
-// ///////////////////////////////////////////////////////////////////////
-// function display_post_tags() {
+// Rename Default "Category" Taxonomy to "Topics"
+//////////////////////////////////////////////////////////////////////
+function sos_chapter_change_cat_label() {
+    global $submenu;
+    $submenu['edit.php'][15][0] = 'Topics'; // Rename categories to Topics
+}
+add_action( 'admin_menu', 'sos_chapter_change_cat_label' );
 
-//     $tags = get_tags();
-//     $html = '<div class="post_tags"> Tagged: ';
-//     foreach ( $tags as $tag ) {
-//         $tag_link = get_tag_link( $tag->term_id );
-                
-//         $html .= "<a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug} btn btn-sm btn-outline-secondary'>";
-//         $html .= "{$tag->name}</a> ";
-//     }
-//     $html .= '</div>';
-//     echo $html;
-// }
+function sos_chapter_change_cat_object() {
+    global $wp_taxonomies;
+    $labels = &$wp_taxonomies['product_cat']->labels;
+    $labels->name = 'Topic';
+    $labels->singular_name = 'Topic';
+    $labels->add_new = 'Add Topic';
+    $labels->add_new_item = 'Add Topic';
+    $labels->edit_item = 'Edit Topic';
+    $labels->new_item = 'Topic';
+    $labels->view_item = 'View Topic';
+    $labels->search_items = 'Search Topics';
+    $labels->not_found = 'No Topics found';
+    $labels->not_found_in_trash = 'No Topics found in Trash';
+    $labels->all_items = 'All Topics';
+    $labels->menu_name = 'Topic';
+    $labels->name_admin_bar = 'Topic';
+}
+add_action( 'init', 'sos_chapter_change_cat_object' );
 
 @include 'inc/widgets.php';
 @include 'inc/breadcrumbs.php';
